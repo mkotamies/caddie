093f72e086ba0c2df63596b5a342bcb30115ffe6
Klaus Julin <klaus.julin@gmail.com>
2014-07-15 23:35:39 +0300

    Shippable conf

--- src/main/resources/static/karma.ci.js
+++ src/main/resources/static/karma.ci.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
 
         coverageReporter: {
             type : 'cobertura',
-            dir : 'shippable/codecoverage',
+            dir : 'shippable/codecoverage/',
             file: '../coverage.xml'
         },
 
